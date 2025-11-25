bdbddf403a04ed992df02619cdb435747519d04c
unknown <kweherialeni@gmail.com>
2025-11-25 11:10:30 +0300

    added empoyees page

--- src/Components/Recycle.vue
+++ src/Components/Recycle.vue
@@ -7,17 +7,44 @@ const router = useRouter()
 <v-row>  
   <v-col md="4">
 <v-card color="teal">
-   <v-img src="public/images/mendros-creative-studio-9MdrCyFKOkM-unsplash.jpg"></v-img>
-<v-card-title> bottle name</v-card-title>
-<v-card-text>bottle available for recycling</v-card-text>
+   <v-img src="images/bottles/derrick-payton-s6xYsISI9Zk-unsplash.jpg"></v-img>
+<v-card-title> Sprite energy drink</v-card-title>
+<v-card-text>bottle available for recycling and points</v-card-text>
 
 </v-card>
   </v-col>
 
  <v-col md="4">
 <v-card color="teal">
-   <v-img src="public/images/nick-fewings--2lJGRIY5P0-unsplash.jpg"></v-img>
-<v-card-title> bottle name</v-card-title>
+   <v-img src="images/bottles/easylife-designs-IJlyaf4q0_s-unsplash.jpg"></v-img>
+<v-card-title> Cocacola</v-card-title>
+<v-card-text>bottle available for recycling</v-card-text>
+
+</v-card>
+  </v-col>
+
+   <v-col md="4">
+<v-card color="teal">
+  <v-img src="images/bottles/erik-mclean-5JdKoyIKWW4-unsplash.jpg"></v-img>
+<v-card-title> Mountain Dew</v-card-title>
+<v-card-text>bottle available for recycling</v-card-text>
+
+</v-card>
+  </v-col>
+
+   <v-col md="4">
+<v-card color="teal">
+  <v-img src="images/bottles/shen-liu-J-UKLgHEotw-unsplash.jpg"></v-img>
+<v-card-title> Wisers Beer </v-card-title>
+<v-card-text>bottle available for recycling</v-card-text>
+
+</v-card>
+  </v-col>
+
+   <v-col md="4">
+<v-card color="teal">
+  <v-img src="images/bottles/shen-liu-J-UKLgHEotw-unsplash.jpg"></v-img>
+<v-card-title> Wisers beer</v-card-title>
 <v-card-text>bottle available for recycling</v-card-text>
 
 </v-card>
@@ -26,34 +53,7 @@ const router = useRouter()
    <v-col md="4">
 <v-card color="teal">
   <v-img src="public/images/nikhil-82LJQZGwW5o-unsplash.jpg"></v-img>
-<v-card-title> bottle name</v-card-title>
-<v-card-text>bottle available for recycling</v-card-text>
-
-</v-card>
-  </v-col>
-
-   <v-col md="4">
-<v-card color="teal">
-  <v-img src="public/images/nikhil-82LJQZGwW5o-unsplash.jpg"></v-img>
-<v-card-title> bottle name</v-card-title>
-<v-card-text>bottle available for recycling</v-card-text>
-
-</v-card>
-  </v-col>
-
-   <v-col md="4">
-<v-card color="teal">
-  <v-img src="public/images/nikhil-82LJQZGwW5o-unsplash.jpg"></v-img>
-<v-card-title> bottle name</v-card-title>
-<v-card-text>bottle available for recycling</v-card-text>
-
-</v-card>
-  </v-col>
-
-   <v-col md="4">
-<v-card color="teal">
-  <v-img src="public/images/nikhil-82LJQZGwW5o-unsplash.jpg"></v-img>
-<v-card-title> bottle name</v-card-title>
+<v-card-title> Pepsi</v-card-title>
 <v-card-text>bottle available for recycling</v-card-text>
 
 </v-card>
